c51b3e281e266b41692f986ad2f8c47ce4fc82c8
Nersisiian <nersisyangrish13@gmail.com>
2026-07-28 18:10:20 +0400

    Fix conftest.py encoding

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -1,11 +1,11 @@
-import pytest_asyncio
+п»їimport pytest_asyncio
 import os
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker
 from app.db.base import Base
 from app.main import app
 from httpx import AsyncClient, ASGITransport
 
-# В CI используется postgres:16 c POSTGRES_DB=testdb, иначе локально testdb
+# Р’ CI РёСЃРїРѕР»СЊР·СѓРµС‚СЃСЏ postgres:16 c POSTGRES_DB=testdb, РёРЅР°С‡Рµ Р»РѕРєР°Р»СЊРЅРѕ testdb
 TEST_DATABASE_URL = os.getenv("TEST_DATABASE_URL", "postgresql+asyncpg://postgres:postgres@localhost:5432/testdb")
 
 test_engine = create_async_engine(TEST_DATABASE_URL, echo=False)
